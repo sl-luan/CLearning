d604c0b601901fe5b324d225cce95fc17d9a3aeb
sl-luan <99403749+sl-luan@users.noreply.github.com>
2025-07-07 17:13:15 +0800

    Add files via upload

---
+++ 打印Helloworld.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//先加载 
+//# 钥匙 
+//include 包含：包括 stdio.h std 标准 standard i输入 input 输入
+//o output 输出 h head 头文件 一种文件格式 .mp4视频格式 .mp3 .flac 音频格式
+//.png .jpg 图片
+//.c 这是C语言的代码文件 .h C语言的独特的头文件
+//行注释 fhmhjokkhhghn 一行之前只要有// 意味着这一行随便写
+
+//main主要的，一个程序中只有一个main，不同文件中也不可以。 
+int main(void) {
+	printf("Hello World!\n");
+	//print打印 print 打印 f format 格式化打印 \n代表是换行
+	//结束（返回） 
+	return 0;
+}
